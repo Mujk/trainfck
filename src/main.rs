9612368ee3a05a7baf8409d561fde3770da2cf1f
Florian Mujkanovic <fminimal@proton.me>
2023-02-25 19:31:54 +0100

    Update interpreter -> Add train crash

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 
+#[derive(Clone)]
 struct Train {
-    dirc: [i8; 2],
-    futr_dirc: [i8; 2],
-    pos: [i32; 2],
+    dirc: Vec<i8>,
+    futr_dirc: Vec<i8>,
+    pos: Vec<usize>,
 }
 
 fn read_file(filename: &str) -> io::Result<Vec<String>> {
     let file = File::open(filename)?;
     let file_reader = BufReader::new(file);
-
     Ok(file_reader.lines().filter_map(io::Result::ok).collect())
 }
 
@@ -40,14 +40,52 @@ fn find_stations(code: Vec<String>, symbol: char) -> Vec<Vec<usize>> {
         if !stations[row].is_empty() {
             stations[row].push(row);
         }
-        let mut stat: Vec<i32>;
         row = row + 1;
     }
     stations.retain(|x| !x.is_empty());
     stations // 0 = x, 1 = y
 }
 
+fn check_crashed(trains: &mut Vec<Train>) -> &Vec<Train> {
+    let mut positions = vec![];
+    for train in &mut *trains {
+        positions.push(train.pos.clone());
+    }
+    positions.sort();
+    let all_pos: Vec<Vec<usize>> = positions.clone();
+    positions.dedup();
+    let mut i = 0;
+    let mut crashed = vec![];
+    for pos in all_pos {
+        if i < positions.len() && pos.eq(&positions[i]) {
+            crashed.push(pos);
+            continue;
+        }
+        i = i + 1;
+    }
+    let crash_num = crashed.len() as i32;
+    crashed.sort();
+    crashed.dedup();
+    i = 0;
+    let loop_trains = trains.clone();
+    for train in loop_trains {
+        for crash in &crashed {
+            if &train.pos == crash {
+                trains.remove(i);
+            }
+        }
+    }
+    trains
+}
+
+fn operators(train: &mut Train, opr: char) -> &Train {
+    match opr {
+        '^' => train.futr_dirc[1] = 1,
+    }
+}
+
 fn main() -> io::Result<()> {
+    #[allow(unused)]
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         panic!("Expected 1 file, got {}", (args.len() - 1).to_string())
@@ -56,24 +94,22 @@ fn main() -> io::Result<()> {
     let code = read_file(&args[1])?;
     let stations = find_stations(code, '+');
     let mut trains: Vec<Train> = Vec::new();
-    let trains_num = stations.len();
+    let mut trains_num = stations.len() as i32;
     let dircs: Vec<Vec<i8>> = vec![vec![1, 0], vec![-1, 0], vec![0, 1], vec![0, -1]];
-    for curr_station in stations {
+    for curr_station in &stations {
         let mut dirc_var = 0;
         while dirc_var < 4 {
             trains.push(Train {
-                dirc: dircs[dirc_var].try_into().unwrap(),
-                futr_dirc: dircs[dirc_var].try_into().unwrap(),
-                pos: curr_station.try_into().unwrap(),
+                dirc: dircs[dirc_var].clone(),
+                futr_dirc: dircs[dirc_var].clone(),
+                pos: curr_station.clone(),
             });
             dirc_var = dirc_var + 1;
         }
     }
-    //let cells: Vec<i8> = Vec::new();
-    let mut not_crashed = true;
-    while not_crashed {
-        println!("Hi");
-        not_crashed = false;
+    let cells: Vec<i8> = Vec::new();
+    while trains_num > 0 {
+        let trains = check_crashed(&mut trains);
     }
     Ok(())
 }
