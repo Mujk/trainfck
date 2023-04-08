2b1455e91bf7ea1eb90f2260ca822b064fd551ab
Florian Mujkanovic <fminimal@proton.me>
2023-04-08 05:20:00 +0200

    Update refactor improve readability

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,16 @@ use std::io::{BufRead, BufReader};
 
 #[derive(Clone)]
 struct Train {
-    dirc: Vec<i8>,
-    futr_dirc: Vec<i8>,
-    pos: Vec<usize>,
-    ignore: bool,
+    direction: Vec<i8>,
+    future_direction: Vec<i8>,
+    position: Vec<usize>,
+    skip: bool,
+}
+
+#[derive(Clone)]
+struct Memory {
+    cells: Vec<u8>,
+    position: usize,
 }
 
 fn read_file(filename: &str) -> io::Result<Vec<String>> {
@@ -19,7 +25,7 @@ fn read_file(filename: &str) -> io::Result<Vec<String>> {
     Ok(file_reader.lines().filter_map(io::Result::ok).collect())
 }
 
-fn check_ext(filename: &str) {
+fn check_extention(filename: &str) {
     let splitted: Vec<&str> = filename.split(".").collect();
     if splitted[splitted.len() - 1] == ".trainf" {
         panic!(
@@ -55,10 +61,10 @@ fn find_stations(code: &Vec<String>, symbol: char) -> Vec<Vec<usize>> {
     stations // 0 = x, 1 = y
 }
 
-fn remove_crashed(mut trains: Vec<Train>, pos: &Vec<usize>) -> Vec<Train> {
+fn remove_crashed(mut trains: Vec<Train>, position: &Vec<usize>) -> Vec<Train> {
     let mut i = 0;
     for train in trains.clone() {
-        if train.pos.eq(pos) {
+        if train.position.eq(position) {
             trains.remove(i);
             continue;
         }
@@ -70,120 +76,122 @@ fn remove_crashed(mut trains: Vec<Train>, pos: &Vec<usize>) -> Vec<Train> {
 fn check_crashed(mut trains: Vec<Train>) -> (i32, Vec<Train>) {
     let mut positions = vec![];
     for train in &trains {
-        positions.push(train.pos.clone());
+        positions.push(train.position.clone());
     }
     positions.sort();
     let mut i: usize = 1;
-    for pos in &positions {
+    for position in &positions {
         if i == positions.len() {
             break;
         }
-        if pos.eq(&positions[i]) {
-            trains = remove_crashed(trains, &pos);
+        if position.eq(&positions[i]) {
+            trains = remove_crashed(trains, &position);
         }
         i = i + 1;
     }
     (trains.len() as i32, trains)
 }
 
-fn turn_dirc(mut dirc: Vec<i8>) -> Vec<i8> {
-    dirc[0] = dirc[0] * -1;
-    dirc[1] = dirc[1] * -1;
-    dirc
+fn turn_direction(mut direction: Vec<i8>) -> Vec<i8> {
+    direction[0] = direction[0] * -1;
+    direction[1] = direction[1] * -1;
+    direction
 }
 
-fn move_pos(mut pos: Vec<usize>, dirc: &Vec<i8>) -> Vec<usize> {
-    for i in 0..dirc.len() {
-        pos[i] = (pos[i] as i32 + dirc[i] as i32) as usize;
+fn move_position(mut position: Vec<usize>, direction: &Vec<i8>) -> Vec<usize> {
+    for i in 0..direction.len() {
+        position[i] = (position[i] as i32 + direction[i] as i32) as usize;
     }
-    pos
+    position
 }
 
-fn get_opr(code: &Vec<String>, pos: &Vec<usize>) -> char {
-    if pos[1] >= code.len() || pos[0] >= code[pos[1]].len() {
+fn get_operator(code: &Vec<String>, position: &Vec<usize>) -> char {
+    if position[1] >= code.len() || position[0] >= code[position[1]].len() {
         ' '
     } else {
-        code[pos[1]].chars().nth(pos[0]).unwrap()
+        code[position[1]].chars().nth(position[0]).unwrap()
     }
 }
 
-fn change_val(val: u8, change: i8) -> u8 {
-    match val as i16 - change as i16 {
+fn change_value(value: u8, change: i8) -> u8 {
+    match value as i16 - change as i16 {
         -1 => 255,
         256 => 0,
-        _ => (val as i8 - change) as u8,
+        _ => (value as i8 - change) as u8,
     }
 }
 
-fn change_cell(mut cell_pos: i32, mut cells: Vec<u8>, dirc_0: usize) -> (Vec<u8>, usize) {
-    cell_pos = cell_pos + dirc_0 as i32;
-    if cell_pos < 0 {
-        cells = [vec![0], cells].concat();
-        cell_pos = 0;
-    } else if cell_pos + 1 - cells.len() as i32 > 0 {
-        cells.push(0);
+fn change_cell(mut memory: Memory, direction_0: usize) -> Memory {
+    let new_cell_position = (memory.position + direction_0) as i32;
+    if new_cell_position < 0 {
+        memory.cells = [vec![0], memory.cells].concat();
+        memory.position = 0;
+        return memory
     }
-    (cells, cell_pos as usize)
+    memory.position = new_cell_position as usize; 
+    if memory.position + 1 - memory.cells.len() > 0 {
+        memory.cells.push(0);
+    }
+    memory
 }
 
 fn operators(
     train: &Train,
-    opr: &char,
-    mut cell_pos: usize,
-    mut cells: Vec<u8>,
-) -> (Train, Vec<u8>, usize) {
+    operator: &char,
+    mut memory: Memory,
+) -> (Train, Memory) {
     let mut this_train = train.clone();
-    if opr.to_owned() == '|' {
-        if this_train.dirc[1] == 0 {
-            this_train.dirc = turn_dirc(this_train.dirc);
+    if operator.to_owned() == '|' {
+        if this_train.direction[1] == 0 {
+            this_train.direction = turn_direction(this_train.direction);
         }
-        return (this_train, cells, cell_pos);
-    } else if opr.to_owned() == '-' {
-        if this_train.dirc[0] == 0 {
-            this_train.dirc = turn_dirc(this_train.dirc);
+        return (this_train, memory);
+    } else if operator.to_owned() == '-' {
+        if this_train.direction[0] == 0 {
+            this_train.direction = turn_direction(this_train.direction);
         }
-        return (this_train, cells, cell_pos);
-    } else if opr.to_owned() == ' ' {
-        this_train.dirc = turn_dirc(this_train.dirc);
-        return (this_train, cells, cell_pos);
+        return (this_train, memory);
+    } else if operator.to_owned() == ' ' {
+        this_train.direction = turn_direction(this_train.direction);
+        return (this_train, memory);
     }
-    if this_train.ignore == true {
-        this_train.ignore = false;
-        return (this_train, cells, cell_pos);
+    if this_train.skip == true {
+        this_train.skip = false;
+        return (this_train, memory);
     }
-    match opr {
-        '^' => this_train.futr_dirc = vec![0, -1],
-        'v' => this_train.futr_dirc = vec![0, 1],
-        '>' => this_train.futr_dirc = vec![1, 0],
-        '<' => this_train.futr_dirc = vec![-1, 0],
-        'o' => this_train.dirc = this_train.futr_dirc.clone(),
+    match operator {
+        '^' => this_train.future_direction = vec![0, -1],
+        'v' => this_train.future_direction = vec![0, 1],
+        '>' => this_train.future_direction = vec![1, 0],
+        '<' => this_train.future_direction = vec![-1, 0],
+        'o' => this_train.direction = this_train.future_direction.clone(),
         '+' => {
-            if this_train.dirc[0] != 0 {
-                (cells, cell_pos) =
-                    change_cell(cell_pos as i32, cells, this_train.dirc[0] as usize);
+            if this_train.direction[0] != 0 {
+                memory =
+                    change_cell(memory.clone(), this_train.direction[0] as usize);
             } else {
-                cells[cell_pos] = change_val(cells[cell_pos], this_train.dirc[1]);
+                memory.cells[memory.position] = change_value(memory.cells[memory.position], this_train.direction[1]);
             }
         }
-        '.' => print!("{}", cells[cell_pos] as char),
+        '.' => print!("{}", memory.cells[memory.position] as char),
         ',' => {
             let mut input = String::new();
             std::io::stdin()
                 .read_line(&mut input)
                 .ok()
                 .expect("failed to read input");
-            cells[cell_pos] = input.bytes().nth(0).expect("failed to read byte");
+            memory.cells[memory.position] = input.bytes().nth(0).expect("failed to read byte");
         }
         '?' => {
-            if cell_pos != 0 && cells[cell_pos] == cells[cell_pos - 1]
-                || cell_pos == 0 && cells[255] == cells[0]
+            if memory.position != 0 && memory.cells[memory.position] == memory.cells[memory.position - 1]
+                || memory.position == 0 && memory.cells[255] == memory.cells[0]
             {
-                this_train.ignore = true;
+                this_train.skip = true;
             }
         }
-        _ => panic!("Illegal operator \"{}\" at: {:?} ", opr, this_train.pos),
+        _ => panic!("Illegal operator \"{}\" at: {:?} ", operator, this_train.position),
     }
-    (this_train, cells, cell_pos)
+    (this_train, memory)
 }
 
 fn rand_train(trains_num: i32) -> Vec<i32> {
@@ -198,34 +206,36 @@ fn main() -> io::Result<()> {
     if args.len() != 2 {
         panic!("Expected 1 file, got {}", (args.len() - 1).to_string())
     }
-    check_ext(&args[1]);
+    check_extention(&args[1]);
     let code = read_file(&args[1])?;
     let stations = find_stations(&code, '+');
+    let mut memory = Memory {
+        cells: vec![0],
+        position: 0,
+    };
     let mut trains: Vec<Train> = Vec::new();
     let dircs: Vec<Vec<i8>> = vec![vec![1, 0], vec![-1, 0], vec![0, 1], vec![0, -1]];
     for curr_station in &stations {
         let mut dirc_var = 0;
         while dirc_var < 4 {
             trains.push(Train {
-                dirc: dircs[dirc_var].clone(),
-                futr_dirc: dircs[dirc_var].clone(),
-                pos: curr_station.clone(),
-                ignore: false,
+                direction: dircs[dirc_var].clone(),
+                future_direction: dircs[dirc_var].clone(),
+                position: curr_station.clone(),
+                skip: false,
             });
             dirc_var = dirc_var + 1;
         }
     }
-    let mut cell_pos: usize = 0;
-    let mut cells: Vec<u8> = vec![0];
     let mut trains_num = stations.len() as i32 * 4;
     while trains_num > 0 {
         let trains_order = rand_train(trains_num);
         for i in trains_order {
-            trains[i as usize].pos =
-                move_pos(trains[i as usize].pos.clone(), &trains[i as usize].dirc);
-            let opr = get_opr(&code, &trains[i as usize].pos.clone());
-            (trains[i as usize], cells, cell_pos) =
-                operators(&trains[i as usize], &opr, cell_pos, cells.clone());
+            trains[i as usize].position =
+                move_position(trains[i as usize].position.clone(), &trains[i as usize].direction);
+            let operator = get_operator(&code, &trains[i as usize].position.clone());
+            (trains[i as usize], memory) =
+                operators(&trains[i as usize], &operator, memory.clone());
         }
         (trains_num, trains) = check_crashed(trains);
     }
